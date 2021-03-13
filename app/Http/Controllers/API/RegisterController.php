45b851aaef3deacc43c5591c2e19719438189a81
Nitesh Devnath <nickdevnath007@gmail.com>
2021-03-13 23:53:08 +0530

    login and register changes

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -6,32 +6,15 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\RegisterAuthRequest;
 use App\User;
-use Tymon\JWTAuth\Facades\JWTAuth;
-use Tymon\JWTAuth\Exceptions\JWTException;
-use App\Http\Controllers\API\LoginController;
 
 class RegisterController extends Controller
 {
-    public $loginAfterSignUp = True;
-
-    protected $LoginController;
-
-    public function __construct(LoginController $LoginController)
-    {
-        $this->LoginController = $LoginController;
-    }
-
-
     public function register(RegisterAuthRequest $request)
     {
         $user = new User;
         $user->name = $request->name;
         $user->phone = $request->phone;
         $user->save();
-
-        if($this->loginAfterSignUp) {
-            return $this->LoginController->login($request);
-        }
 
         return response()->json([
            'success' => true,
