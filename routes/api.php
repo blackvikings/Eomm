0b161c45de3e712ca74756da7b859f6281f82b12
Nitesh Devnath <nickdevnath007@gmail.com>
2021-03-13 11:50:50 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\API\ProductContoller;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('products', 'API/ProductController@list');
+Route::get('products', 'ProductController@list');
