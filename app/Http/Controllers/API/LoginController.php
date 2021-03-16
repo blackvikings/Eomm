f4eb9f4ea34578b0c8cf4edc5ac15d01c6518ccf
Nitesh Devnath <nickdevnath007@gmail.com>
2021-03-16 17:55:23 +0530

    login and register changes

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
-use JWTAuth;
-use Tymon\JWTAuth\Exceptions\JWTException;
 
 class LoginController extends Controller
 {
