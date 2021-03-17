90a58af4f19dfeb45a0c0342312671c3f9c33a5e
Nitesh Devnath <nickdevnath007@gmail.com>
2021-03-17 17:23:03 +0530

    login and register changes

--- app/Http/Controllers/API/PrescriptionController.php
+++ app/Http/Controllers/API/PrescriptionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Prescription;
+use App\User;
 use Validator;
 
 class PrescriptionController extends Controller
