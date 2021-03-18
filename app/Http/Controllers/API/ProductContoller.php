a21676f8b27eeb68db9a8ff1fa246c192a2a896d
Nitesh Devnath <nickdevnath007@gmail.com>
2021-03-18 10:01:17 +0530

    product search

--- app/Http/Controllers/API/ProductContoller.php
+++ app/Http/Controllers/API/ProductContoller.php
@@ -35,11 +35,8 @@ class ProductContoller extends Controller
         }
         $res = Product::all();
         $cat = Category::all();
-         return response()->json([
-           'category'=> $category,
-           'name' => $name
-         ]);
-        if(isset($category) && isset($name)){
+
+        if(isset($category) && isset($name) && $name != "" && $category != ""){
             $name = strtolower($name);
 //            return "hello";
             $sRes = DB::select( DB::raw("SELECT * FROM `products` WHERE lower(name) like `%{$name}%` and `category_id` = '$category'" ) );
