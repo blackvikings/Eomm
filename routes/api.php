c97c4c0b532a1de17d0f0a8a95ec08bad3353a38
Nitesh Devnath <nickdevnath007@gmail.com>
2021-03-18 14:23:25 +0530

    cart list

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,7 @@ Route::middleware('APIToken')->group(function () {
     // Logout
     Route::post('/logout',[RegisterController::class, 'postLogout']);
     Route::post('upload-prescription', [PrescriptionController::class, 'upload']);
-    Route::get('cart-list', [ProductContoller::class, 'cartList']);
+    Route::get('cart-list/{token}', [ProductContoller::class, 'cartList']);
     Route::post('add-to-cart', [ProductContoller::class, 'addTocart']);
     Route::delete('remove-from-cart/{id}', [ProductContoller::class, 'removeCart']);
 });
